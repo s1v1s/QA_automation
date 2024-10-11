9ef215c51f9153991e051624b9763feb3059db56
Ivan Sivushkov <sivushkovivan@gmail.com>
2024-10-12 00:36:13 +0300

    Начато добавление валидации json

--- tests/test_something.py
+++ tests/test_something.py
@@ -1,9 +1,12 @@
 import requests
 
 from configuration import SERVICE_URL
-from src.enums.global_enums import GlobalErrorMessages
+from src.baseclasses.response import Response
+from src.schemas.post import POST_SCHEMA
+
 
 def test_getting_posts():
-    response = requests.get(url=SERVICE_URL)
-    assert response.status_code == 200, GlobalErrorMessages.WRONG_STATUS_CODE.value
-    print(response.json())
+    r = requests.get(url=SERVICE_URL)
+    response = Response(r)
+
+    response.assert_status_code(200).validate(POST_SCHEMA)
